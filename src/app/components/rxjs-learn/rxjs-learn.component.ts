5d4b14ee1a7402de50e33b3692b9899b4465f282
kitety <aizaizuori@foxmail.com>
2022-03-18 23:56:16 +0800

    feat: from event

--- src/app/components/rxjs-learn/rxjs-learn.component.ts
+++ src/app/components/rxjs-learn/rxjs-learn.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { from, fromEvent, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-learn',
@@ -10,10 +10,20 @@ export class RxjsLearnComponent implements OnInit {
   agents: Observable<string>;
   agentName: string;
 
+  studentList = ['Student 1', 'Student 2', 'Student 3'];
+  students: Observable<string[]> = of(this.studentList);
+
+  // from 传入array like或者array，返回的是一个Observable，但是订阅的是里面的每一个值
+  orders$: Observable<string> = from(['order1', 'order2', 'order3']);
+
+  @ViewChild('validate')
+  validate: ElementRef;
+
   constructor() {}
 
   ngOnInit(): void {
-    this.agents = new Observable<string>(function (subscriber) {
+    // Observable
+    /* this.agents = new Observable<string>(function (subscriber) {
       try {
         subscriber.next('Agent 1');
         setTimeout(() => {
@@ -29,6 +39,29 @@ export class RxjsLearnComponent implements OnInit {
     this.agents.subscribe((data) => {
       console.log('subscribe', data);
       this.agentName = data;
+    }); */
+    // of operator
+    // 创建一个 Observable 对象，并且把数据放入它里面
+    /* console.log(' this.students: ', this.students);
+    this.students.subscribe((data) => {
+      console.log('subscribe', data);
+    }); */
+    // from operator
+    // Array Like
+    // this.orders$.subscribe((data) => {
+    //   console.log('subscribe', data);
+    // });
+    // from event
+  }
+
+  rsjxEventObservable() {
+    console.log(this.validate?.nativeElement);
+    const btnObservable = fromEvent<MouseEvent>(
+      this.validate?.nativeElement,
+      'mouseover'
+    );
+    btnObservable.subscribe((event) => {
+      console.log('event', event);
     });
   }
 }
